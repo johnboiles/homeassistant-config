7bc4638ba69823257cef8bfbcdc9021ed4b7d889
John Boiles <johnaboiles@gmail.com>
2019-11-11 19:47:40 -0800

    Script to bulk-configure zwave wd100 dimmers

---
+++ python_scripts/configure_zwave.py
@@ -0,0 +1,13 @@
+wd200_nodes = [7, 8, 9, 10, 11, 12]
+parameters = {
+    7: 1,
+    8: 10,
+    9: 1,
+    10: 10,
+}
+
+for node in wd200_nodes:
+    for parameter in parameters:
+        service_data = {'node_id': node, 'parameter': parameter, 'value': parameters[parameter]}
+        hass.services.call('zwave', 'set_config_parameter', service_data, True)
+        time.sleep(.2)
